5e2e54d02f6513a7640ebc20c3aacaadc0ce76c9
stevezhere <sfang4@yahoo.com>
2016-10-11 16:03:01 -0500

    added song path and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'musics#index'
+
+  get '/musics', to: 'musics#index'
+  get '/musics/show', to: 'musics#show'
+
+  
+  # resources :musics, only: [:index, :create]
+  # post '/musics', to: 'musics#show'
+
+  # get '/patients/:id', to: 'patients#show'
 end
