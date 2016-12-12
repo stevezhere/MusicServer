0ed51cdd329daa56e7a00d1b420d6f7a0cdd580e
stevezhere <sfang4@yahoo.com>
2016-12-12 11:22:35 -0600

    add create and new music route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'musics#index'
 
-  get '/musics', to: 'musics#index'
+  resources :musics, only: [:index, :create, :new]
   get '/musics/show', to: 'musics#show'
-
   
   # resources :musics, only: [:index, :create]
   # post '/musics', to: 'musics#show'
