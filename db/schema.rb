bd8ac382dc6e66db78458bc524880f6ceeb2f1cf
stevezhere <sfang4@yahoo.com>
2016-12-12 18:18:13 -0600

    create music storage migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161213001125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "music_storages", force: :cascade do |t|
+    t.string   "title"
+    t.string   "file_location"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "musics", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "path",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
