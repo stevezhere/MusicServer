6b1c389f4b46df0e00d07bdd6bc80f7fe65e9ecf
stevezhere <sfang4@yahoo.com>
2017-02-14 00:10:06 -0600

    move music scan into a model function and fix nested routes for /search and /scan.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'musics#index'
 
+  get 'musics/search', to: 'musics#search'
+  get 'musics/scan', to: 'musics#scan'
   resources :musics, only: [:index, :create, :new, :show]
-  get '/search', to: 'musics#search'
-  get '/scan', to: 'musics#scan'
   
   # resources :musics, only: [:index, :create]
   # post '/musics', to: 'musics#show'
