8af0c21b1388e09307024a008ecaa8ac959104e1
stevezhere <sfang4@yahoo.com>
2017-02-27 20:39:31 -0600

    remove some unused routes
    Former-commit-id: b8af55fc2b627d3c8c6bedf2871157b29756af4d

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: :show do
     collection do 
-      resources :playlists
+      resources :playlists, except: [:index, :new]
     end      
   end
 
