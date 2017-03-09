e4b9a379d508aeacbad2329f9fdb71c3c177825d
stevezhere <sfang4@yahoo.com>
2017-03-08 18:55:30 -0600

    add notes for future dependency delete songentry

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,6 @@
 class Playlist < ActiveRecord::Base
 	belongs_to :user
-	has_many :song_entries
+	has_many :song_entries#, :dependent => :delete
 	has_many :musics, through: :song_entries
 	validates_presence_of :name, :user_id
 end
