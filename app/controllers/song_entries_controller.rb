1d51a6f5381d36e23601155d1bd0cfb904acbedc
stevezhere <sfang4@yahoo.com>
2018-01-22 12:43:08 -0600

    modify playlistChecklistForm componenet to add song entry

--- app/controllers/song_entries_controller.rb
+++ app/controllers/song_entries_controller.rb
@@ -3,30 +3,37 @@ class SongEntriesController < ApplicationController
 
 	def create
 		if params[:song_entry]
+			add_songs = []
 			song_ids = params[:song_entry].keys.map{|song_id| song_id.to_i}
 			song_ids.each do |song|
 				entry = SongEntry.new({playlist_id: params[:playlist_id], music_id: song})
-				entry.save if entry.valid?
-			end
-		end
-		redirect_to edit_playlist_path(params[:playlist_id])
-	end
-
-	def destroy
-		if params[:song_entry]
-			removed_song = []
-			song_ids = params[:song_entry].keys.map{|song_id| song_id.to_i}
-			song_ids.each do |song|
-				entry = SongEntry.find_by(playlist_id: params[:playlist_id], music_id: song)
-				removed_song << entry.music if entry
-				entry.destroy if entry
+				add_songs << entry.music if entry.save
 			end
 		end
 		respond_to do |format|
 			format.html { redirect_to edit_playlist_path(params[:playlist_id]) }
 			format.json {
-				return render json: { deleted: removed_song } if removed_song
-				return head 400
+				add_songs ? (render json: { data: add_songs }) : (head 400)
+			}
+		end
+	end
+
+	def destroy
+		if params[:song_entry]
+			remove_songs = []
+			song_ids = params[:song_entry].keys.map{|song_id| song_id.to_i}
+			song_ids.each do |song|
+				entry = SongEntry.find_by(playlist_id: params[:playlist_id], music_id: song)
+				if entry
+					remove_songs << entry.music
+					entry.destroy
+				end
+			end
+		end
+		respond_to do |format|
+			format.html { redirect_to edit_playlist_path(params[:playlist_id]) }
+			format.json {
+				remove_songs ? (render json: { data: remove_songs }) : (head 400)
 			}
 		end
 	end
