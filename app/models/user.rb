80183fdcc45deb24614d4b781c7e8ce30757cb31
stevezhere <sfang4@yahoo.com>
2018-02-02 13:04:50 -0600

    change user model inheritance to be consistance with other models
    Former-commit-id: c30b0c5bdfbcf2d7cac7ed0ad7133bcd8612ab2e

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable
   has_many :playlists
+
+  def move_to(user)
+  	self.playlists.update_all(user_id: user.id)
+  end
 end
