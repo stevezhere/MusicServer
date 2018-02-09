06e49a56590cf7baffc4c3ba4afec3d62512b637
stevezhere <sfang4@yahoo.com>
2018-02-09 13:54:29 -0600

    allow user/show access with visibilty limitation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!, unless: -> { current_user.guest? }
 	def show
-		if @user = User.find_by_id(current_user)
-			@playlists = @user.playlists.order(:created_at)
-			@playlist = Playlist.new
-			render 'show'
+		if @user = User.find_by_id(params[:id])
+			if @user == current_user
+				@playlists = @user.playlists.order(:created_at)
+				@playlist = Playlist.new
+			else
+				@playlists = @user.playlists.where(share: true).order(:created_at)
+			end
+				render 'show'
 		else
-			redirect_to '/', flash: { alert: 'Unauthorized access' }
+			redirect_back(fallback_location: playlists_path, flash: {notice: 'User Not Found'}) 
 		end
 	end
 end
