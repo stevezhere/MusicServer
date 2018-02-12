9868c3a5fec75dc582e5015bc648025085229454
stevezhere <sfang4@yahoo.com>
2018-02-11 23:38:57 -0600

    create audioPlayer component and implement into react index only
    Former-commit-id: 069a287485aacbde33ad2f3b7386d3bb0b27d1d9

---
+++ app/assets/javascripts/components/musics/audio_player.jsx
@@ -0,0 +1,59 @@
+class AudioPlayer extends React.Component {
+	musicTitle() {
+		if(this.props.musicEmpty) {
+			return "EMPTY SONGLIST";
+		} else {
+			return this.props.musics[0].title;
+		}
+	}
+
+	musicStream() {
+		if(!this.props.musicEmpty) {
+			return `/musics/${this.props.musics[0].id}/stream`;
+		} else {
+			return "";
+		}
+	}
+
+	emptyList() {
+		if(this.props.musicEmpty) {
+			return <li> "No Songs Available" </li>;
+		}
+	}
+
+	render() {
+		return(
+			<div>
+				<div className='container playerDiv'>
+					<div className="row">
+						<div className="col-xs-6 col-xs-offset-3 titleContainer">
+							<h2 className="songTitle">
+								{ this.musicTitle() }
+							</h2>
+						</div>
+					</div>
+					<div className="row trackButtonsRow">
+						<div className="col-xs-1 col-xs-offset-4">
+								<button id='previous' className={`isDisabled-${this.props.musicEmpty}`}>
+									<img src="/assets/rewind.png" alt="rewind" size="15" />
+								</button>
+						</div>
+						<div className="col-xs-1 col-xs-offset-2">
+							<button id='next' className={`isDisabled-${this.props.musicEmpty}`}>
+								<img src="/assets/fast-forward.png" alt="forward" size="15"/>
+							</button>
+						</div>		
+					</div>
+					<div className="row audioRow">
+						<div className="col-xs-1 col-xs-offset-3">
+							<audio id='audioPlayer' controls>
+								<source src={ this.musicStream() }/>
+							</audio>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
