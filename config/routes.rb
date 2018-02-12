1ef4acf73102c77563bf59966d529d532b628735
stevezhere <sfang4@yahoo.com>
2018-02-12 00:30:37 -0600

    merge musics new with index and delete routes musics#new

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,13 @@ Rails.application.routes.draw do
   resources :users, only: :show
 
   resources :playlists, except: [:new] do
-
     post '/song_entries/create', to: 'song_entries#create'
     delete '/song_entries/destroy', to: 'song_entries#destroy'
   end
 
-  resources :musics, except: [:edit, :update] do 
+  resources :musics, except: [:new, :edit, :update] do 
   	collection do
-  		get :search
+  		# get :search
       get '/:id/stream', to: 'musics#stream', as: 'stream'
   	end
   end
