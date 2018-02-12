e45a9eeacb6c469da102b298901b1c2a0eff0f07
stevezhere <sfang4@yahoo.com>
2018-02-12 03:13:03 -0600

    add tempPlaylist function on homepage
    Former-commit-id: 5d3a4eb7b625fe70168d0b52b06fdc6e99437465

--- app/assets/javascripts/components/musics/musics.jsx
+++ app/assets/javascripts/components/musics/musics.jsx
@@ -1,10 +1,17 @@
 class Musics extends React.Component {
 	constructor(props){
 		super(props);
-		this.state = {musics: this.props.musics, deleteToggle: false};
+		this.state = {
+			musics: this.props.musics, 
+			tempPlaylist: [], 
+			playlistToggle: false, 
+			deleteToggle: false
+		};
 		this.addMusic = this.addMusic.bind(this);
 		this.deleteMusic = this.deleteMusic.bind(this);
+		this.addToPlaylist = this.addToPlaylist.bind(this);
 		this.handleDeleteToggle = this.handleDeleteToggle.bind(this);
+		this.handlePlaylistToggle = this.handlePlaylistToggle.bind(this);
 	}
 
 	sortList(list) {
@@ -36,7 +43,23 @@ class Musics extends React.Component {
 	}
 
 	handleDeleteToggle() {
-		this.setState({deleteToggle: !this.state.deleteToggle});
+		this.setState({
+			deleteToggle: !this.state.deleteToggle, 
+			playlistToggle: false
+		});
+	}
+
+	addToPlaylist(music) {
+		let tempPlaylist = this.state.tempPlaylist.slice();
+		tempPlaylist.push(music);
+		this.setState({tempPlaylist: tempPlaylist});
+	}
+
+	handlePlaylistToggle() {
+		this.setState({
+			playlistToggle: !this.state.playlistToggle,
+			deleteToggle: false
+		});
 	}
 
 	trashButton() {
@@ -47,13 +70,42 @@ class Musics extends React.Component {
 		}
 	}
 
+	streamMusic() {
+		if(this. playlistToggle) {
+			return this.state.tempPlaylist;
+		} else {
+			return this.state.musics;
+		}
+	}
+
+	showPlaylist() {
+		debugger;
+		if(this.state.playlistToggle) {
+			return(
+				<div>
+					<h2>Temp Playlist</h2>
+					<ol>
+						{this.state.tempPlaylist.map( (music) =>
+							<li>
+								{music.title}
+							</li>	
+						)}
+					</ol>
+				</div>
+			)
+		}
+	}
+
 	render() {
 		return(
 			<div>
 				<h1> Local Music Server Homepage </h1>
 				<br/><br/>
-				<AudioPlayer musics={this.state.musics} musicEmpty={!this.state.musics.length}/>
-				<br/>
+				<AudioPlayer musics={this.streamMusic()} musicEmpty={!this.state.musics.length}/>
+				{this.showPlaylist()}
+				<center>
+					<button onClick={this.handlePlaylistToggle}>Create Temp Playlist</button>
+				</center>
 				<div title="Only Users may Add/Delete Music">
 					<MusicForm handleNewMusic={this.addMusic} guest={this.props.guest}/>
 					<button onClick={this.handleDeleteToggle}> 
@@ -65,9 +117,11 @@ class Musics extends React.Component {
 					{this.state.musics.map( (music, idx) =>
 						<li className={`musicSource ${idx}`} key={music.id}>
 							<MusicLink music={music} guest={this.props.guest} 
-								toggle={this.state.deleteToggle}
+								deleteToggle={this.state.deleteToggle}
 								handleDeleteMusic={this.deleteMusic} 
-								handleDeleteToggle={this.handleDeleteToggle} />
+								handleDeleteToggle={this.handleDeleteToggle}
+								playlistToggle={this.state.playlistToggle}
+								handlePlaylistAdd={this.addToPlaylist}/>
 						</li>
 					)}
 				</ul>
