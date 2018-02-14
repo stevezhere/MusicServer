a92a5f05ef01afda40e1b4db6bd5968505b257c5
stevezhere <sfang4@yahoo.com>
2018-02-13 21:58:27 -0600

    create seperate TempPlaylist component and make list class more dynamic
    Former-commit-id: 382d66800451e34f7a28b82d660736623be9f672

---
+++ app/assets/javascripts/components/playlists/temp_playlist.jsx
@@ -0,0 +1,35 @@
+class TempPlaylist extends React.Component {
+	listClass() {
+		if(this.props.playable) {
+			return 'songList searchableList';
+		} else {
+			return 'searchableList';
+		}
+	}
+
+	showPlaylist() {
+		if(this.props.musics.length || this.props.playable) {
+			return(
+				<div>
+					<hr/>
+					<h2>Temporary Playlist</h2>
+					<ol className={this.listClass()}>
+						{this.props.musics.map( (music, idx) =>
+							<li className={`musicSource ${idx}`} key={music.id}>
+								<a href={`/musics/${music.id}`}>
+									{music.title}
+								</a>
+							</li>	
+						)}
+					</ol>
+				</div>
+			);
+		} else {
+			return <div></div>;
+		}
+	}
+
+	render() {
+		return this.showPlaylist();
+	}
+}
