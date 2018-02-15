07d1f24c85cac54c1d29d3b255ef3aea76615ac8
stevezhere <sfang4@yahoo.com>
2018-02-15 13:52:42 -0600

    create save temp playlist feature
    Former-commit-id: a87e1d1545aed71c8237ff648c5444365a676ca4

--- app/assets/javascripts/components/playlists/temp_playlist.jsx
+++ app/assets/javascripts/components/playlists/temp_playlist.jsx
@@ -2,11 +2,43 @@ class TempPlaylist extends React.Component {
 	constructor() {
 		super();
 		this.handleRemoveSong = this.handleRemoveSong.bind(this);
+		this.savePlaylist = this.savePlaylist.bind(this);
 	}
 
 	handleRemoveSong(e) {
 		e.preventDefault();
 		this.props.handlePlaylistRemove(parseInt(e.target.className));
+	}
+
+	savePlaylist(e) {
+		e.preventDefault();
+		$.ajax({
+			url: '/playlists',
+			method: 'POST',
+			dataType: 'JSON',
+			beforeSend: (xhr) => {
+				xhr.setRequestHeader(
+					'X-CSRF-Token', 
+					$('meta[name="csrf-token"]').attr('content')
+				)
+			},
+			data: {playlist: {name: `List - ${new Date().toLocaleString()}`}},
+			success: (r) => {
+				let entry_ids = this.props.musics.map( music => music.id );
+				let data = {song_entry: {entry_ids: entry_ids}};
+		  	$.ajax({
+		  		url: `/playlists/${r.id}/song_entries/create`,
+		  		method: 'POST',
+		  		beforeSend: (xhr) => {
+						xhr.setRequestHeader(
+							'X-CSRF-Token', 
+							$('meta[name="csrf-token"]').attr('content')
+						)
+					},
+					data: data
+				});
+			}
+		});
 	}
 
 	activeClass() {
@@ -27,6 +59,9 @@ class TempPlaylist extends React.Component {
 				<div>
 					<hr/>
 					<h2 className={this.activeClass()['activeList']}>Temporary Playlist</h2>
+					<button title="Redirect Playlist Settings" onClick={this.savePlaylist}> 
+						Save Playlist 
+					</button>
 					<ol className={this.activeClass()['songList']}>
 						{this.props.musics.map( (music, idx) =>
 							<li className={`musicSource source-${music.id}`} key={music.id} title='Remove Song'>
